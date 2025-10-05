d13fd4e1b46532fcc84fc779e82c4ad01e77d09d
prosper <shifttechgs1@gmail.com>
2025-10-05 14:19:58 +0200

    website files

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,146 @@
+<!-- Start Footer Area -->
+<div class="footer-area bg-color-0c3a30 pt-120">
+    <div class="container">
+        <div class="row" data-cues="slideInUp" data-duration="800">
+            <div class="col-xl-4 col-lg-9 col-md-6">
+                <div class="footer-weight">
+                    <h2 class="text-white">Subscribe Newsletter</h2>
+                    <form class="footer-form position-relative">
+                        <input type="email" class="form-control" placeholder="Enter Your Email">
+                        <button type="submit" class="default-btn">Subscribe <i class="ri-arrow-right-up-line"></i></button>
+                    </form>
+                    <ul class="social">
+                        <li><span>Follow Us:</span></li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <i class="ri-facebook-fill"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.twitter.com/" target="_blank">
+                                <i class="ri-twitter-x-line"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/" target="_blank">
+                                <i class="ri-instagram-line"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank">
+                                <i class="ri-linkedin-fill"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-2 col-lg-4 col-md-6">
+                <div class="footer-weight part-two ps-5">
+                    <h3 class="text-white">Quick Links</h3>
+
+                    <ul class="service-link">
+                        <li>
+                            <a href="terms-conditions.html">Terms & Conditions</a>
+                        </li>
+                        <li>
+                            <a href="blog.html">Blog & News</a>
+                        </li>
+                        <li>
+                            <a href="services.html">Mobile App</a>
+                        </li>
+                        <li>
+                            <a href="services.html">Why Choose Us</a>
+                        </li>
+                        <li>
+                            <a href="pricing.html">Pricing Plan</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+                <div class="footer-weight part-three ps-5">
+                    <h3 class="text-white">Our Services</h3>
+
+                    <ul class="service-link">
+                        <li>
+                            <a href="services.html">Mobile Banking</a>
+                        </li>
+                        <li>
+                            <a href="services.html">Advanced Security</a>
+                        </li>
+                        <li>
+                            <a href="services.html">Digital Wallet</a>
+                        </li>
+                        <li>
+                            <a href="services.html">Budgeting Tools</a>
+                        </li>
+                        <li>
+                            <a href="services.html">Making Transactions</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+                <div class="footer-weight ps-5">
+                    <h3 class="text-white">Get In Touch</h3>
+
+                    <ul class="get-touch">
+                        <li>
+                            <img src="assets/images/svg/map.svg" alt="image">
+                            <span class="text-white"><b>Location:</b></span>
+                            <a href="https://www.google.com/maps/search/18+Tanglewood+Lane+Gulfport/@30.4216847,-89.1511032,12z/data=!3m1!4b1?entry=ttu" target="_blank">18 Tanglewood Lane Gulfport</a>
+                        </li>
+                        <li>
+                            <img class="phone" src="assets/images/svg/mail.svg" alt="image">
+                            <span class="text-white"><b>Email:</b></span>
+                            <a href="https://templates.hibootstrap.com/cdn-cgi/l/email-protection#9af2fff6f6f5dafcf3f4eef5b4f9f5f7"><span class="__cf_email__" data-cfemail="5139343d3d3e1137383f253e7f323e3c">[email&#160;protected]</span></a>
+                        </li>
+                        <li>
+                            <img class="phone" src="assets/images/svg/phone.svg" alt="image">
+                            <span class="text-white"><b>Phone:</b></span>
+                            <a href="tel:0018085550148">+001 (808) 5550148</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Footer Area -->
+
+<!-- Start Copyright Area -->
+<div class="copyright-area bg-color-0c3a30">
+    <div class="container">
+        <div class="copyright-border">
+            <div class="row g-4 align-items-center">
+                <div class="col-lg-4 col-md-6">
+                    <p>© <script>document.write(new Date().getFullYear())</script> <span>PayHouse Finance</span> Developed By <a href="https://shifttechgs.com/" target="_blank">ShiftTech</a></p>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="copy-image" data-cues="zoomIn" data-duration="700">
+                        <a href="index.html" class="d-block">
+                            <img src="assets/images/white-logo.png" alt="image">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <ul>
+                        <li>
+                            <a href="privacy-policy.html">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="terms-conditions.html">Terms & Conditions</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Copyright Area -->
+
+<!-- Go top Btn -->
+<div class="go-top">
+    <i class="ri-arrow-up-fill"></i>
+</div>
+<!-- Go top Btn End -->
